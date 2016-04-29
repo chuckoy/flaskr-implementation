48f3d822b1a196c6754f6ce471138a67033509d0
Chuck Logan Lim <chucklogan.lim@gmail.com>
2016-04-30 09:00:33 +1000

    Add code for setting up database connection
    Connection is made before request and torn down once a response has been
    made.

--- flaskr.py
+++ flaskr.py
@@ -9,14 +9,32 @@ app.config.from_object('settings')
 
 
 def connect_db():
+    """Connect to the database specified in configuration"""
     return sqlite3.connect(app.config['DATABASE'])
 
 
 def init_db():
+    """Initialise the database"""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before every request"""
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    """
+    Close connection to the database after a response has been constructed
+    """
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
